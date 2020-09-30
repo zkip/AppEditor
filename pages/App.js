24839c7d13cc99e5ebef2938ab8519f7387d1224
zkip <zkiplan@qq.com>
2020-09-30 18:21:26 +0800

    ...

--- pages/App.js
+++ pages/App.js
@@ -1,95 +1,29 @@
 import Outline from "@/components/Outline";
 import { node_map, dom_payload_map } from "@/components/Outline";
+import { useOutlineReducer } from "@/components/Outline/Outline";
+import { makeOutlineState } from "@/components/Outline/Outline";
+import { useMapTo } from "@/components/Outline/Outline";
 import { listen } from "@/utils/fn";
 import { inBound } from "public/workspace/Box/utils/array";
 import { useEffect, useRef, useState } from "react";
 
-const instances = {
-	id: "001",
-	name: "Jackie",
-	index: 0,
-	level: 0,
-	children: [
-		{
-			id: "002",
-			name: "Tom",
-			index: 0,
-			level: 1,
-			children: [
-				{ id: "003", name: "Tee", index: 1, level: 2 },
-				{ id: "005", name: "Juice", index: 2, level: 2 },
-				{ id: "008", name: "Alice", index: 3, level: 2 },
-				{ id: "007", name: "Bob", index: 4, level: 2 },
-				{ id: "090", name: "Jip", index: 5, level: 2 },
-				{ id: "022", name: "Thor", index: 6, level: 2 },
-				{ id: "055", name: "Iss", index: 7, level: 2 },
-				{
-					id: "004",
-					name: "Hufed",
-					index: 0,
-					level: 2,
-					children: [{ id: "007", name: "Jud", index: 0, level: 3 }],
-				},
-			],
-		},
-	],
+const Item = ({ name }) => {
+	return <div className="Item">{name}</div>;
 };
 
-const instances_2 = {
-	id: "004",
-	name: "Jackie",
-	index: 0,
-	children: [
-		{
-			id: "005",
-			name: "Tom",
-			index: 3,
-			children: [{ id: "006", name: "Tee" }],
-		},
-		{ id: "002", name: "Hufed", index: 1 },
-		{ id: "025", name: "Iop", index: 0 },
-		{ id: "017", name: "Fup", index: 2 },
-	],
-};
-
-function getNodePayload(path) {
-	let should_node,
-		is_found = false,
-		found_index = 0;
-
-	for (let i = 0; i < 2; i++) {
-		should_node = path[i];
-		if (dom_payload_map.has(should_node)) {
-			// Found.
-			is_found = true;
-			found_index = i;
-			break;
-		}
-	}
-
-	if (is_found) {
-		// current payload
-		const payload = dom_payload_map.get(should_node);
-		const parentPayload_dom =
-			should_node.parentNode && should_node.parentNode.parentNode;
-		// parent payload
-		const parentPayload = dom_payload_map.get(parentPayload_dom);
-
-		if (should_node.children[0] === path[found_index - 1]) {
-			// title parts
-		} else {
-			// children parts
-		}
-
-		return {
-			payload,
-			parentPayload,
-		};
-	} else {
-		// Not found.
-	}
-	return {};
-}
+const instances = [
+	{ level: 0, Child: Item, payload: { id: "001", name: "Jackie" } },
+	{ level: 1, Child: Item, payload: { id: "002", name: "Tom" } },
+	{ level: 2, Child: Item, payload: { id: "003", name: "Tee" } },
+	{ level: 2, Child: Item, payload: { id: "005", name: "Juice" } },
+	{ level: 2, Child: Item, payload: { id: "008", name: "Alice" } },
+	{ level: 2, Child: Item, payload: { id: "007", name: "Bob" } },
+	{ level: 2, Child: Item, payload: { id: "090", name: "Jip" } },
+	{ level: 2, Child: Item, payload: { id: "022", name: "Thor" } },
+	{ level: 2, Child: Item, payload: { id: "055", name: "Iss" } },
+	{ level: 2, Child: Item, payload: { id: "004", name: "Hufed" } },
+	{ level: 3, Child: Item, payload: { id: "014", name: "Jud" } },
+];
 
 function getDOMBound(dom) {
 	const styles = getComputedStyle(dom);
@@ -135,152 +69,24 @@ const lineHeight = 20;
 
 export default () => {
 	const [ins, setIns] = useState(instances);
-	const [payload, setPayload] = useState();
-	const [actived_payload, setActivePayload] = useState({
-		dom: null,
-		name: "",
-		raw: {},
-	});
-
 	const [DragComponent, setDragComponent] = useState(() => ThumbDragger);
 	const [thumbDraggerProps, setThumbDraggerProps] = useState({});
+	const outlineReducer = useOutlineReducer(makeOutlineState(instances));
+	// console.log(outlineReducer[0], "---");
 
-	const outline_ref = useRef();
+	// const outline_ref = useRef();
 
-	useEffect(
-		() => {
-			console.log("@@@@");
-			return listen("mousedown")(({ path, clientX, clientY }) => {
-				const { payload, parentPayload } = getNodePayload(path);
-				console.log(parentPayload, "@@@@@@@@s");
-				if (payload) {
-					const { dom } = payload;
-					const bound = getDOMBound(dom.children[0]);
-					const node_self_payload = {
-						...payload.raw,
-						children: [],
-					};
-
-					const ix = clientX,
-						iy = clientY;
-
-					const putThumb = () => {
-						setDragComponent(() => ThumbDragger);
-						setThumbDraggerProps({
-							...thumbDraggerProps,
-							bound,
-							outline_props: { data: node_self_payload },
-						});
-					};
-
-					const init_index = payload.raw.index;
-					const parentPayloadChildren = parentPayload.children;
-
-					let created = false;
-
-					const outline_bound = outline_ref.current.getBoundingClientRect();
-
-					const clean_move = listen("mousemove")(
-						({ path, clientX, clientY }) => {
-							const { payload } = getNodePayload(path);
-
-							const livePayloadChildren = parentPayloadChildren.slice();
-
-							if (!created) {
-								created = true;
-								putThumb();
-							}
-
-							if (payload) {
-							}
-
-							const local_offset =
-								(iy - outline_bound.y) % lineHeight;
-
-							const absolute_index =
-								(clientY - outline_bound.y - local_offset) /
-								lineHeight;
-
-							// console.log(local_offset, absolute_index);
-
-							const live_payload =
-								livePayloadChildren[init_index - 1];
-
-							live_payload.index = absolute_index;
-
-							// const [_payload] = livePayloadChildren.splice(
-							// 	init_index,
-							// 	1
-							// );
-							// _payload.index = absolute_index;
-							// livePayloadChildren.splice(
-							// 	absolute_index,
-							// 	0,
-							// 	_payload
-							// );
-
-							console.log(
-								livePayloadChildren,
-								"----",
-								absolute_index
-							);
-
-							const dx = clientX - ix,
-								dy = clientY - iy;
-
-							const live_bound = {
-								...bound,
-								x: bound.x,
-								y: bound.y + dy,
-							};
-
-							const max_level_current = 10;
-
-							const level = inBound(
-								0,
-								max_level_current + 1
-							)(node_self_payload.level + dx / indent);
-
-							setThumbDraggerProps({
-								...thumbDraggerProps,
-								bound: live_bound,
-								outline_props: {
-									data: { ...node_self_payload, level },
-								},
-							});
-
-							parentPayload.raw.children = livePayloadChildren;
-
-							// setIns({ ...ins });
-						}
-					);
-
-					const clean_up = listen("mouseup")(() => {
-						// parentPayload.children = parentPayload.raw.children;
-						clean_move();
-						clean_up();
-						setDragComponent(() => () => null);
-					});
-				}
-			});
-		},
-		[
-			// actived_payload.dom,
-			// ins,
-			// setIns,
-			// thumbDraggerProps,
-			// setThumbDraggerProps,
-		]
-	);
+	// const [OutlineMapped, mappers] = useMapTo(Outline);
+	// console.log(OutlineMapped, mappers);
 	return (
 		<div>
 			<div className="DragLayer">
 				<DragComponent {...thumbDraggerProps} />
 			</div>
 			<div>
-				<Outline data={ins} ref={outline_ref} />
+				<Outline reducer={outlineReducer} />
 			</div>
-			<pre>{actived_payload.name.toString()}</pre>
+			<pre>{""}</pre>
 		</div>
 	);
 };
--- src/components/Outline/Outline.jsx
+++ src/components/Outline/Outline.jsx
@@ -1,7 +1,24 @@
 import { joinBySpace } from "@/utils/array";
-import { useRef, useEffect, forwardRef } from "react";
+import { listen } from "@/utils/fn";
+import { useRef, useEffect, forwardRef, useState, useReducer } from "react";
 import "./Outline.css";
 
+const Item = ({ name }) => {
+	return <div className="Item">{name}</div>;
+};
+const instances = [
+	{ level: 0, Child: Item, payload: { id: "001", name: "Jackie" } },
+	{ level: 1, Child: Item, payload: { id: "002", name: "Tom" } },
+	{ level: 2, Child: Item, payload: { id: "003", name: "Tee" } },
+	{ level: 2, Child: Item, payload: { id: "005", name: "Juice" } },
+	{ level: 2, Child: Item, payload: { id: "008", name: "Alice" } },
+	{ level: 2, Child: Item, payload: { id: "007", name: "Bob" } },
+	{ level: 2, Child: Item, payload: { id: "090", name: "Jip" } },
+	{ level: 2, Child: Item, payload: { id: "022", name: "Thor" } },
+	{ level: 2, Child: Item, payload: { id: "055", name: "Iss" } },
+	{ level: 2, Child: Item, payload: { id: "004", name: "Hufed" } },
+	{ level: 3, Child: Item, payload: { id: "014", name: "Jud" } },
+];
 export const node_map = {};
 export const dom_payload_map = new Map();
 
@@ -48,14 +65,145 @@ const Node = ({ data, top_payload }) => {
 	);
 };
 
-export default forwardRef(
-	({ data = { name: "", children: [] }, className, style, ...rest }, ref) => {
-		const cls = joinBySpace(className, "Outline");
+export function getPayload() {}
 
-		return (
-			<div className={cls} ref={ref}>
-				<Node data={data} top_payload={data} />
+const FlatNode = forwardRef(({ Child, level, payload, ...rest }, ref) => {
+	return (
+		<div className="node" ref={ref}>
+			<div className="title" style={{ paddingLeft: `${level * 20}px` }}>
+				<Child {...payload} />
 			</div>
-		);
-	}
-);
+		</div>
+	);
+});
+
+const lineHeight = 20;
+
+export const makeOutlineState = (data = []) => {
+	return {
+		data,
+		records: new Map(),
+	};
+};
+
+export const useOutlineReducer = (initState = makeOutlineState()) => {
+	const reducer = useReducer((state, action) => {
+		if (action.type === "delete") {
+			const { index } = action;
+			const { data } = state;
+			state.data.splice(index, 1);
+			return { ...state, data };
+		} else if (action.type === "save") {
+			const { id } = action;
+			const { records, data } = state;
+			records.set(id, data.slice());
+			return { ...state, records };
+		} else if (action.type === "restore") {
+			const { id } = action;
+			const { records } = state;
+			records.delete(id);
+			return { ...state, records };
+		} else if (action.type === "setBasedOn") {
+			const { id, affect } = action;
+			const data = affect(state.records.get(id).slice());
+			return { ...state, data };
+		} else {
+			return state;
+		}
+	}, initState);
+	return reducer;
+};
+
+export default ({
+	reducer = useOutlineReducer(),
+	className,
+	style,
+	...rest
+}) => {
+	const ref = useRef();
+	const cls = joinBySpace(className, "Outline");
+	const [path_index, setPahtIndex] = useState(0);
+	const [Node, mapper] = useNodeIndexer();
+	const [state, dispatch] = reducer;
+
+	useEffect(() => {
+		if (!ref) return;
+		const top_node_dom = ref.current;
+		const top_bound = top_node_dom.getBoundingClientRect();
+		return listen("mousedown", top_node_dom, { capture: false })((e) => {
+			const { path, clientY } = e;
+			const item_dom = path[path.length - path_index - 3];
+			const payload = mapper.get(item_dom);
+
+			const local_offset = (clientY - top_bound.y) % lineHeight;
+			const init_index =
+				(clientY - top_bound.y - local_offset) / lineHeight;
+
+			const id = Symbol();
+
+			dispatch({ type: "save", id });
+
+			const clean_move = listen("mousemove")(({ clientY }) => {
+				const local_offset = (clientY - top_bound.y) % lineHeight;
+				const live_index =
+					(clientY - top_bound.y - local_offset) / lineHeight;
+
+				dispatch({
+					type: "setBasedOn",
+					id,
+					affect(data) {
+						const [payload] = data.splice(init_index, 1);
+						data.splice(live_index, 0, payload);
+						return data;
+					},
+				});
+			});
+
+			const clean_up = listen("mouseup")(() => {
+				clean_move();
+				clean_up();
+				dispatch({ type: "restore", id });
+			});
+		});
+	}, [path_index, ref.current, dispatch]);
+
+	useEffect(() => {
+		if (!ref) return;
+		const top_node_dom = ref.current;
+
+		let index = 0,
+			parent_node = top_node_dom.parentNode;
+		while ((index++, !!(parent_node = parent_node.parentNode)));
+
+		setPahtIndex(index);
+	}, [ref && ref.current]);
+
+	return (
+		<div className={cls} ref={ref}>
+			{state.data.map(({ Child, level, payload }) => (
+				<Node key={payload.id} {...{ Child, level, payload }} />
+			))}
+		</div>
+	);
+};
+
+const useNodeIndexer = () => {
+	const dom_payload_map = new Map();
+	const FlatNodeMapped = useMapTo(FlatNode, (dom, { payload }) => {
+		dom_payload_map.set(dom, payload);
+		return () => {
+			dom_payload_map.delete(dom);
+		};
+	});
+	return [FlatNodeMapped, dom_payload_map];
+};
+
+export function useMapTo(Component, indexer) {
+	return ({ ...props }) => {
+		const ref = useRef();
+		useEffect(() => {
+			return indexer(ref.current, props);
+		}, ref.current);
+		return <Component {...props} ref={ref} />;
+	};
+}
