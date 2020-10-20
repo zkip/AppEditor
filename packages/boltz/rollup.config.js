f85c5bb9fc3031e9f77303dc027178771bf8a64c
zkip <zkiplan@qq.com>
2020-10-20 21:53:26 +0800

    ...

---
+++ packages/boltz/rollup.config.js
@@ -0,0 +1,22 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import zResolve from "@zrlps/rollup-plugin-resolve";
+import pkg from "./package.json";
+
+export default {
+	input: "src/index.js",
+	output: {
+		file: pkg.main,
+		format: "umd",
+		name: "boltz-ui",
+	},
+	plugins: [
+		svelte(),
+		zResolve({
+			candidateExt: ["svelte"],
+		}),
+		resolve(),
+		commonjs(),
+	],
+};
---
+++ packages/boltz/src/components/Test/Test.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from "svelte";
+
+	let i = 0;
+
+	onMount(() => {
+		setInterval(() => {
+			i++;
+		}, 1000);
+	});
+</script>
+
+<div>count: {i}.</div>
